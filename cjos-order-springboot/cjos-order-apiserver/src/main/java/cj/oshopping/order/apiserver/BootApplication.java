8abd9ddf2d602066451d5d0259d575888c25a271
mspark <mspark7233@gmail.com>
2016-03-08 15:41:31 +0900

    각 서비스의 모듈(front-web, apiserver)에 선언된 JsonConfig를 websupport로 이동 BootApplication에서 import하여 사용하기를 권장

--- cjos-order-springboot/cjos-order-apiserver/src/main/java/cj/oshopping/order/apiserver/BootApplication.java
+++ cjos-order-springboot/cjos-order-apiserver/src/main/java/cj/oshopping/order/apiserver/BootApplication.java
@@ -2,10 +2,14 @@ package cj.oshopping.order.apiserver;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
 
+import cj.oshopping.common.config.JsonConfig;
+import cj.oshopping.common.crypto.spring.PropertiesConfiguration;
 import cj.oshopping.common.yaml.YamlSupportEnvironment;
 
 @SpringBootApplication
+@Import(value = { PropertiesConfiguration.class, JsonConfig.class })
 public class BootApplication {
 
 	public static void main(String[] args) {
---
+++ cjos-order-springboot/cjos-order-apiserver/src/main/java/cj/oshopping/order/apiserver/config/WebConfig.java
@@ -0,0 +1,10 @@
+package cj.oshopping.order.apiserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import cj.oshopping.common.webapp.api.version.config.ApiWebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfig extends ApiWebMvcConfigurationSupport{
+
+}
